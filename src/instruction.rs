287053ee3fda4cac10be5006aa54336f9ca8b411
Leonard Kulms <leonard.kulms@icloud.com>
2022-03-19 16:12:09 +0100

    implement instruction

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,3 +1,8 @@
+use std::convert::TryInto;
+use solana_program::program_error::ProgramError;
+
+use crate::error::EscrowError::InvalidInstruction;
+
 pub enum EscrowInstruction {
 
     /// Starts the trade by creating and populating an escrow account and transferring ownership of the given temp token account to the PDA
@@ -14,5 +19,28 @@ pub enum EscrowInstruction {
     InitEscrow {
         /// The amount party A expects to receive of token Y
         amount: u64
+    }
+}
+
+impl EscrowInstruction {
+    /// Unpacks a byte buffer into a [EscrowInstruction](enum.EscrowInstruction.html).
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
+
+        Ok(match tag {
+            0 => Self::InitEscrow {
+                amount: Self::unpack_amount(rest)?,
+            },
+            _ => return Err(InvalidInstruction.into()),
+        })
+    }
+
+    fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
+        let amount = input
+            .get(..8)
+            .and_then(|slice| slice.try_into().ok())
+            .map(u64::from_le_bytes)
+            .ok_or(InvalidInstruction)?;
+        Ok(amount)
     }
 }
